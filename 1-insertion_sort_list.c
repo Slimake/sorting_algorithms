cadb0fae99f6e5a74c58cf231d1645a14c32eecc
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-26 02:38:01 +0000

    Add 1-insertion_sort_list.c file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts integers using Insertion sort.
+ * @list: The doubly linked list of integers.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	int found = 0;
+	listint_t *tmp, *tmp_nxt = NULL;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+	tmp = *list;
+	while (tmp->next)
+	{
+		if (tmp->n > tmp->next->n)
+		{
+			tmp->next->prev = tmp->prev;
+			if (tmp->next->prev)
+				tmp->prev->next = tmp->next;
+			else
+				*list = tmp->next;
+
+			tmp->prev = tmp->next;
+			tmp->next = tmp->next->next;
+			tmp->prev->next = tmp;
+
+			if (tmp->next)
+				tmp->next->prev = tmp;
+			tmp = tmp->prev;
+			print_list(*list);
+
+			if (tmp->prev && (tmp->prev->n > tmp->n))
+			{
+				if (!found)
+					tmp_nxt = tmp->next;
+				found = 1;
+				tmp = tmp->prev;
+				continue;
+			}
+		}
+		if (!found)
+			tmp = tmp->next;
+		else
+			tmp = tmp_nxt, found = 0;
+	}
+}
