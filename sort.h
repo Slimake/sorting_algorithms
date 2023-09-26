6189f43f86c9e86a8f0c8055c35c7cb894490972
Sunday Ajakaiye <marcusitovie@gmail.com>
2023-09-26 03:43:12 +0000

    Update sort.h header file

--- sort.h
+++ sort.h
@@ -31,5 +31,6 @@ void quick_sort(int *array, size_t size);
 void quick_sort_rep(int *array, int low, int high, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
 #endif /* end of SORT_H */
